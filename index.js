0dfa818699813ce9cf10f10adcdd40f66085ba29
shyamvadaliya12 <73700344+shyamvadaliya12@users.noreply.github.com>
2023-10-03 10:25:26 +0530

    Update

--- index.js
+++ index.js
@@ -12,4 +12,17 @@ function getCountry(name) {
     }
 }
 
+
+function getState(name) {
+    if (!name) {
+        return countryJSON;
+    } else {
+        let newArray = countryJSON.filter(function (el) {
+            return el.name.toLocaleLowerCase() === name.toLocaleLowerCase()
+        });
+        return newArray;
+    }
+}
+
 module.exports.getCountry = getCountry;
+module.exports.getState = getState;
